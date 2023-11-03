c1596f821ee03d6dfb53bb9b623b08eb66b289fa
peter6070 <peter6070@naver.com>
2023-11-04 00:41:58 +0900

    install react-router-dom
    라우터 설치

--- greendrive-kyungjun/src/App.js
+++ greendrive-kyungjun/src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* <Routes>
+          <Route path="/" element={<Firstpage />} />
+          <Route path="/Join" element={<Join />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes> */}
+        <LandingPage />
+      </div>
+    </BrowserRouter>
   );
 }
 
