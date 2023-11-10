951b8408d3b6238801496c681cac1fb960da89a0
leestana01 <leestana01@naver.com>
2023-11-11 03:03:37 +0900

    [FIX] 공공데이터 포털 연동 완료

--- src/pages/Review/ReviewHome.js
+++ src/pages/Review/ReviewHome.js
@@ -145,6 +145,23 @@ const ReviewHome = (props) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  
+  const [publicSpaceName, setPublicSpaceName] = useState('');
+  const [publicSpaceId, setPublicSpaceId] = useState("100-1-000004");
+  const [publicSpaceType, setPublicSpaceType] = useState('');
+  const [publicSpaceTime, setPublicSpaceTime] = useState('');
+  const [publicPhoneNumber, setPublicPhoneNumber] = useState('');
+  const [publicAddress, setPublicAddress] = useState('');
+  const [publicBasicTime, setPublicBasicTime] = useState('');
+  const [publicBasicCharge, setPublicBasicCharge] = useState('');
+  const [publicAddUnitTime, setPublicAddUnitTime] = useState('');
+  const [publicAddUnitCharge, setPublicAddUnitCharge] = useState('');
+  const [publicDayTicketTime, setPublicDayTicketTime] = useState('');
+  const [publicDayTicketCharge, setPublicDayTicketCharge] = useState('');
+  const [publicMonthTicketCharge, setPublicMonthTicketCharge] = useState('');
+  const [publicPlusInfo, setPublicPlusInfo] = useState('');
+
+  const PUBLIC_DATA_KEY = process.env.REACT_APP_PUBLIC_DATA_KEY;
 
   const handleReviewIconClick = () => {
     setIsReviewSelected(true);
@@ -159,21 +176,53 @@ const ReviewHome = (props) => {
 
   const fetchData = async () => {
     try {
-      setError(null);
-      setData(null);
-      setLoading(true);
+        setError(null);
+        setData(null);
+        setLoading(true);
+      await axios
+        .get(`http://api.data.go.kr/openapi/tn_pubr_prkplce_info_api`, {
+              params: {
+                serviceKey: decodeURIComponent(PUBLIC_DATA_KEY),
+                pageNo: 1,
+                numOfRows: 10,
+                type : "JSON",
+                prkplceNo : publicSpaceId
+              }
+            })
+        .then((response) => {
+          // 디버그용 출력문
+          console.log("공공데이터 포털 데이터 정상 응답 :", response.data);
+          setData(response.data);
 
-      const response = await axios.get(URL, {
-        params: {
-          serviceKey: process.env.REACT_APP_API_KEY,
-          numOfRows: 1,
-          pageNo: 10
-        }
-      });
+          const publicData = response.data.response.body.items[0];
+          console.log("주차장 기반 응답 : ",publicData);
 
-      setData(response.data);
-    } catch (e) {
-      setError(e);
+          const today = new Date();
+          const dayOfWeek = today.getDay();
+          if (dayOfWeek >= 1 && dayOfWeek <= 5) { // 주중
+            setPublicSpaceTime(`주중 운영 시간: ${publicData.weekdayOperOpenHhmm} ~ ${publicData.weekdayOperColseHhmm}`);
+          } else if (dayOfWeek === 6) { // 토요일
+            setPublicSpaceTime(`토요일 운영 시간: ${publicData.satOperOperOpenHhmm} ~ ${publicData.satOperCloseHhmm}`);
+          } else { // 공휴일 (=일요일)
+            setPublicSpaceTime(`공휴일 운영 시간: ${publicData.holidayOperOpenHhmm} ~ ${publicData.holidayCloseOpenHhmm}`);
+          }
+          setPublicSpaceName(publicData.prkplceNm);                   // 주차장 이름
+          setPublicSpaceType(publicData.prkplceSe);                   // 주차장 유형 (공영, 민영)
+          setPublicPhoneNumber(publicData.phoneNumber);               // 전화번호
+          setPublicAddress(publicData.rdnmadr !== "" ? publicData.rdnmadr : publicData.lnmadr); // 주차장 주소 (도로명 우선)
+          setPublicBasicTime(publicData.basicTime);                   // 주차 기본 시간
+          setPublicBasicCharge(publicData.basicCharge);               // 주차 기본 요금
+          setPublicAddUnitTime(publicData.addUnitTime);               // 추가 단위 시간
+          setPublicAddUnitCharge(publicData.addUnitCharge);           // 추가 단위 요금
+          setPublicDayTicketTime(publicData.dayCmmtktAdjTime);        // 1일주차권 요금적용시간
+          setPublicDayTicketCharge(publicData.dayCmmtkt);             // 1일주차권 요금
+          setPublicMonthTicketCharge(publicData.monthCmmtkt);         // 월 정기권 요금
+          setPublicPlusInfo(publicData.spcmnt);                       // 특이사항 (할인 관련 추가정보)
+
+        });
+    } catch (error) {
+      console.error("공공데이터 포털 응답 오류:", error);
+      setError(error);
     }
     setLoading(false);
   };
@@ -185,8 +234,6 @@ const ReviewHome = (props) => {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error...</div>;
   if (!data) return null;
-
-  const prkplceNm = data.response.body.items.item.rdnmadr;
   return (
     <div>
       <Container>
@@ -196,7 +243,7 @@ const ReviewHome = (props) => {
               <Logo>GreenDriver</Logo>
             </MyHeader>
             {/* <TitleLineA /> */}
-            <Title>{prkplceNm}</Title> 
+            <Title>{publicSpaceName}</Title> 
 
             <Iconbox>
             <Reviewicon
@@ -228,11 +275,11 @@ const ReviewHome = (props) => {
             <Separator1 isSelected={isReviewSelected} />
             <Separator2 isSelected={isBookmarkSelected} />
           </Iconbox>
-          <p>주소 : { data.response.body.items.item.rdnmadr }</p>
-          <p>시간 : { data.response.body.items.item.operDay }</p>
-        <p>전화번호 : { data.response.body.items.item.parkingchrgeInfo }</p>
-       <p>요금: { data.response.body.items.item.phoneNumber }</p>
-        <p>특이사항 : { data.response.body.items.item.spcmnt }</p>
+          <p>주소 : { publicAddress}</p>
+          <p>시간 : { publicSpaceTime }</p>
+        <p>전화번호 : { publicPhoneNumber }</p>
+       <p>요금: { publicBasicCharge }</p>
+        <p>특이사항 : { publicPlusInfo }</p>
 
           </Body>
         </BodyWrapper>
