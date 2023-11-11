f5130372e6b0a600eea8ee7959cd130a3190e27f
peter6070 <peter6070@naver.com>
2023-11-12 04:00:31 +0900

    update Nav
    화면 전환 수정

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -271,8 +271,8 @@ const Mypage = () => {
   const [resLength, setLength] = useState(0);
   const [userId, setUserId] = useState(localStorage.getItem("userId"));
   const [isOpen1, setIsOpen1] = useState(false);
-  const [isReviewSelected, setIsReviewSelected] = useState(false);
-  const [isBookmarkSelected, setIsBookmarkSelected] = useState(true);
+  const [isReviewSelected, setIsReviewSelected] = useState(true);
+  const [isBookmarkSelected, setIsBookmarkSelected] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
   const [profileImage, setProfileImage] = useState(null);
   const [isJudged, setIsJudged] = useState(localStorage.getItem("isJudged"));
@@ -386,6 +386,11 @@ const Mypage = () => {
     }
     changeMessage();
   }, [isJudged]);
+
+  useEffect(() => {
+    localStorage.setItem("home", false);
+  }, []);
+  
 
   const [isDropDown, setIsDropDown] = useState(true);
   function handleDropDown() {
